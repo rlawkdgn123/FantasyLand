f90750859ba8f1581dd54147f43ec0496c9e3d9b
rlawkdgn123 <63436456+rlawkdgn123@users.noreply.github.com>
2020-10-30 13:34:45 +0900

    20201030 - 2

--- Assets/02.Scrpits/LookItem.cs
+++ Assets/02.Scrpits/LookItem.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class LookItem : MonoBehaviour,IPointerEnterHandler,
-    IPointerExitHandler,IGvrPointerHoverHandler
+public class LookItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IGvrPointerHoverHandler
 {
+
+    private Animator anim;
+    private readonly int hashIsOpen = Animator.StringToHash("IsOpen");
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void OnBoxOpen(bool isOpen)
+    {
+        anim.SetBool(hashIsOpen, isOpen);
+    }
+
     public void OnLookItemBox(bool isLookAt)
     {
         Debug.Log(isLookAt);
         MoveCtrl.isStopped = isLookAt;
     }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         MoveCtrl.isStopped = true;
@@ -23,4 +36,5 @@ public class LookItem : MonoBehaviour,IPointerEnterHandler,
     {
         Debug.Log("Reticle On");
     }
+
 }
